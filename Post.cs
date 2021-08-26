23a244434a389cdf1686cd85c644a08a7b84c133
DapperProgrammer <74994236+DapperProgrammer@users.noreply.github.com>
2021-08-26 14:39:51 -0500

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Overflow_Post
+{
+    class Post
+    {
+        private string _Title, _Description;
+        private int _TotalUpVote, _TotalDownVote;
+        private DateTime _CreationDate;
+
+        // Default Constructor
+        public Post()
+        {
+
+        }
+
+        public void SetTitle(string title)
+        {
+            _Title = title;
+            _CreationDate = DateTime.Now;
+        }
+
+        public string GetTitle()
+        {
+            return _Title;
+        }
+
+        public void SetDescription(string desc)
+        {
+            _Description = desc;
+        }
+
+        public string GetDescription()
+        {
+            return _Description;
+        }
+
+        public DateTime GetDate()
+        {
+            return _CreationDate;
+        }
+
+        public void SetUpVote()
+        {
+            _TotalUpVote = _TotalUpVote + 1;
+        }
+
+        public int GetUpvoteCount()
+        {
+            return _TotalUpVote;
+        }
+
+        public void SetDownVote()
+        {
+            _TotalUpVote = _TotalUpVote - 1;
+            _TotalDownVote = _TotalDownVote + 1;
+        }
+
+        public int GetDownVoteCount()
+        {
+            return _TotalDownVote;
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Stack_Overflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post StackOverflow = new Post();
+
+            Console.WriteLine("Enter post Title: ");
+            var userTitle = Console.ReadLine();
+            StackOverflow.SetTitle(userTitle);
+
+            Console.WriteLine("Description: \n");
+            var userDescription = Console.ReadLine();
+            StackOverflow.SetDescription(userDescription);
+
+            Console.WriteLine("Title: {0} \nDate: {1} \nDescription {2}", StackOverflow.GetTitle(), StackOverflow.GetDate(), StackOverflow.GetDescription());
+
+            var run = true;
+            while (run == true)
+            {
+                Console.WriteLine("Press Y to upvote, N to downvote or S to exit: ");
+                var userChoice = Console.ReadLine();
+
+                if (userChoice == "y")
+                {
+                    StackOverflow.SetUpVote();
+                    Console.WriteLine("Total UpVotes: {0}", StackOverflow.GetUpvoteCount());
+                    Console.WriteLine("Total DownVotes: {0}", StackOverflow.GetDownVoteCount());
+                }
+                else if (userChoice == "n")
+                {
+                    StackOverflow.SetDownVote();
+                    Console.WriteLine("Total UpVotes: {0}", StackOverflow.GetUpvoteCount());
+                    Console.WriteLine("Total DownVotes: {0}", StackOverflow.GetDownVoteCount());
+                }
+                else if (userChoice == "s")
+                {
+                    run = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error try again.");
+                }
+            }
+
+        }
+    }
+}
